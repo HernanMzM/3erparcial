edcad2d0bc6f65408d1acc4ed72b5f1022ae49f1
HernanMzM <hernanmum3@gmail.com>
2024-05-26 20:07:03 -0600

    programa 5

---
+++ prog5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"introduzca una letra\n";
+    cin>>c;
+    if (c=='a' || c=='e'||c=='i'||c=='o'||c=='u'){
+        cout<<c<<" es una vocal";
+    }
+}
