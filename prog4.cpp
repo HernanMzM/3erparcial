2667c065a3eb54c65b1d31c19d19258364e0b141
HernanMzM <hernanmum3@gmail.com>
2024-05-26 20:03:46 -0600

    programa 4

---
+++ prog4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+	char c;
+	cout<<"ingrese una letra\n";
+    cin>>c;
+    switch (c)
+    {
+        case'a':
+        cout<<c<<" es una vocal";
+        break;
+        case'e':
+        cout<<c<<" es una vocal";
+        break;
+        case'i':
+        cout<<c<<" es una vocal";
+        break;
+        case'o':
+        cout<<c<<" es una vocal";
+        break; 
+        case'u':
+        cout<<c<<" es una vocal";
+        break;
+        default:
+        break;
+    }
+}
