830441ebc9971193fb37dc200407f7a989f1b158
HernanMzM <hernanmum3@gmail.com>
2024-05-30 20:18:41 -0600

    ya funciona :)

--- 2clase1.cpp
+++ 2clase1.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int num,i=1,cont;
-	int numero[num];
+	int num,i,cont=0;
+	int numero[5];
 	cout<<"qpasa";
 	for(i=1;i<=5;i++){
-		cout<<"num"<<i;
+		cout<<"ingresa un numero "<<i<<":\n";
 		cin>>num;
 		numero[i]=num;
 		if(num==0){
@@ -14,8 +14,8 @@ int main(){
 		}
 		cont=cont+numero[i];
 	}
-	for(i=0;i<=5;i++){
-		cout<<"el en "<<i<<"esta"<<numero[i]<<"\n";
+	for(i=5;i>=1;i--){
+		cout<<"el en "<< i<<" esta "<<numero[i]<<"\n";
 	}
 	cout<<"el total de la suma es"<<cont;
 }
