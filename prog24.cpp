7c1ef670a6e1488412e640aa8a26000d589332b5
HernanMzM <hernanmum3@gmail.com>
2024-06-01 19:50:42 -0600

    programa 24

---
+++ prog24.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char pass[12], name[20];
+    int co, i, precio, cantidad, total, co2;
+    co = 0;
+    co2 = 0;
+    
+    do {
+        co = co + 1;
+        cout << "usuario:\n";
+        cin >> name;
+        cout << "contraseña:\n";
+        cin >> pass;   
+        if (strcmp(pass, "2b26") == 0 && strcmp(name, "Hernan") == 0) {
+            cout << "Bienvenido al sistema\n";
+            break;
+        }
+        if (co >= 3) {
+            cout << "Has excedido el número máximo de intentos\n";
+            return 0;
+        }
+    } while (co < 3);
+
+    do {
+        cout << "introduzca el precio del articulo\n";
+        cin >> precio;
+
+        if (precio < 0) {
+            cout << "ingrese un numero positivo\n";
+            continue;
+        }
+
+        if (precio == 0) {
+            break;
+        }
+
+        cout << "introduzca la cantidad de unidades vendidas\n";
+        cin >> cantidad;
+
+        if (cantidad < 0) {
+            cout << "ingrese un numero positivo\n";
+            continue;
+        }
+
+        total = precio * cantidad;
+        co2 += total;
+
+    } while (cantidad > 0);
+
+    cout << "El importe total es de " << co2 << endl;
+
+    return 0;
+}
