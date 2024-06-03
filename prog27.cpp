1780927c12332f931adc44e479dccd4ecf2d2e9c
HernanMzM <hernanmum3@gmail.com>
2024-06-03 12:19:10 -0600

    los demas programas según los demas son los del anterior pero tienen como 8 versiones de ese y cada uno es un programa :/

---
+++ prog27.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int num[5];
+int numero;
+
+
+for(int i=-0;i<5;i++){
+cout<<"digite el primer numero para la posicion "<<i+1<<endl;
+cin>>numero;
+num[i]=numero;
+}
+for(int i=-0;i<5;i++){
+cout<<"el dato de la posicion"<<i+1<<"es"<<num[1];
+
+}
+return 0;
+}
+//los demas programas según los demas son los del anterior pero tienen como 8 versiones de ese y cada uno es un programa :/
