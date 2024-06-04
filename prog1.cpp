2b8ba0629f10dab6f3f43d4670b13d90cd826764
HernanMzM <hernanmum3@gmail.com>
2024-06-03 20:12:03 -0600

    cambios que me señaló que hiciera

--- prog1.cpp
+++ prog1.cpp
@@ -8,7 +8,7 @@ int main(){
 	cin>>n2;
 	cout<<"ingresa el tercer numero\n";
 	cin>>n3;
-	if(n1==1 && n2==2 && n3==3){
+	if(n1==n2 && n2==n3 && n3==n1){
 		cout<<"acceso permitido";
 	}else{
 		cout<<"acceso denegado";
